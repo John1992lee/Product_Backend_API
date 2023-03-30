082b5bd4512893c9949a9857719bce4363f92084
John Lee <John1992lee18@yahoo.com>
2023-03-30 17:09:14 -0500

    Created products model for products app

---
+++ products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    inventory_quantity = models.IntegerField
+    price = models.DecimalField(max_digits=3, decimal_places=2)
